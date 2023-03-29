c498c5c36ddc8ff74a228db54e1d7a619ab47841
by-westik <nastya.derevyanko.2020@mail.ru>
2023-03-29 22:42:16 +0300

    Add image

--- app/src/main/java/com/by/westik/compose/android/birthdayapp/MainActivity.kt
+++ app/src/main/java/com/by/westik/compose/android/birthdayapp/MainActivity.kt
@@ -3,14 +3,17 @@ package com.by.westik.compose.android.birthdayapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.by.westik.compose.android.birthdayapp.ui.theme.BirthdayAppTheme
@@ -25,11 +28,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthdayGreetingWithText(message = "Happy Birthday Westik!", from = "from Nastya")
+                    BirthdayGreetingWithImage(message = "Happy Birthday Westik!", from = "from " +
+                            "Nastya")
                 }
             }
         }
     }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+
 }
 
 @Composable
@@ -51,6 +69,6 @@ fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier =
 @Composable
 fun BirthdayCardPreview() {
     BirthdayAppTheme {
-        BirthdayGreetingWithText(message = "Happy Birthday Westik!", from = "from Nastya")
+        BirthdayGreetingWithImage(message = "Happy Birthday Westik!", from = "from Nastya")
     }
 }
