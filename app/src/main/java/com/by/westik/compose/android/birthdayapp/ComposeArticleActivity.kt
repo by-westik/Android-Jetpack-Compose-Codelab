8855095b8007d9d443fbccd767a20f5a0e793df5
by-westik <nastya.derevyanko.2020@mail.ru>
2023-03-31 15:22:05 +0300

    Add ComposeArticleActivity

---
+++ app/src/main/java/com/by/westik/compose/android/birthdayapp/ComposeArticleActivity.kt
@@ -0,0 +1,93 @@
+package com.by.westik.compose.android.birthdayapp
+
+import android.os.Bundle
+import androidx.compose.foundation.Image
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.by.westik.compose.android.birthdayapp.ui.theme.BirthdayAppTheme
+
+class ComposeArticleActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ComposeArticleUtil()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticle(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    painter: Painter,
+    modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = shortDescription,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        Text(
+            text = longDescription,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun ComposeArticleUtil(modifier: Modifier = Modifier) {
+    ComposeArticle(
+        title = stringResource(R.string.article_title),
+        shortDescription = stringResource(R.string.short_description),
+        longDescription = stringResource(R.string.long_description),
+        painter = painterResource(R.drawable.bg_compose_background)
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BirthdayAppTheme {
+        ComposeArticleUtil()
+    }
+}
