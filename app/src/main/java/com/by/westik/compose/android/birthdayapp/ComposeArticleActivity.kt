aa6cc509cf374c8a8c5e792ac960e5a319f191d1
by-westik <nastya.derevaynko.2020@mail.ru>
2023-04-23 14:21:08 +0300

    test commit 3

--- app/src/main/java/com/by/westik/compose/android/birthdayapp/ComposeArticleActivity.kt
+++ app/src/main/java/com/by/westik/compose/android/birthdayapp/ComposeArticleActivity.kt
@@ -21,7 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.by.westik.compose.android.birthdayapp.ui.theme.BirthdayAppTheme
 
-//test commit 2
+//test commit 3
 
 class ComposeArticleActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
